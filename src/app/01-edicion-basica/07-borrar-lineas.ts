575a510488be5742154c4a483de0450fcb736a15
jbenavidesnati05 <jorgenati05@gmail.com>
2022-06-27 16:40:10 -0500

    subiendo

---
+++ src/app/01-edicion-basica/07-borrar-lineas.ts
@@ -0,0 +1,29 @@
+/*
+    Objetivo:
+        Comentar un bloque de código
+
+    Tips:
+        ⇧ ⌘ K
+        Ctrl + Shift + K
+
+    PRO:
+        Seleccionar todas las ocurrencias de la selección
+        ⇧ ⌘ L
+        Ctrl + Shift + L
+*/
+
+let NoMeBorres = ':)';
+let hola = ':(';
+
+NoMeBorres = '1';
+hola    = 'a';
+NoMeBorres = '1';
+hola    = 'a';
+
+
+// Resultado final (sin los comentarios claro.)
+
+// let NoMeBorres = ':)';
+
+// NoMeBorres = '1';
+// NoMeBorres = '1';
